3b6f296415eaf24b11c9d9a47ca45e71295827e2
Tonny <tonny.ortiz.95@gmail.com>
2024-11-26 09:53:11 -0600

    Converted the keras file

--- ModelScripts/convert.py
+++ ModelScripts/convert.py
@@ -2,8 +2,17 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 
 # Carga el modelo TensorFlow.
-model = tf.keras.models.load_model('taylor_swift')
+model = tf.keras.models.load_model('taylor_swift.keras')
+# Re-save the model in the native Keras format.
+model.save('taylor_swift_reformatted.keras')
+
+# Load the reformatted model.
+model = tf.keras.models.load_model('taylor_swift_reformatted.keras')
+
+
+# Check the Keras version from the model metadata
 model.compile()
 
 # Convierte el modelo a JavaScript.
 tfjs.converters.save_keras_model(model, 'taylor_swift_js')
+
